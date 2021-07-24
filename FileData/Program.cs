71f40ea7ea2d6f1fe9a9b49dd9df9bbda49dc2f7
Sonali Dobhal <P10500916@capita.co.uk>
2021-07-24 11:09:11 +0530

    Added coding changes

---
+++ FileData/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using ThirdPartyTools;
+
+namespace FileData
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Type typeInstance = typeof(FileDetails);
+            var firstArgument = !string.IsNullOrEmpty(args[0]) ? Regex.Replace(args[0], "[^0-9a-zA-Z]+", "") : string.Empty;
+            object classInstance = Activator.CreateInstance(typeInstance, null);
+            foreach (MethodInfo methodInfo in typeInstance.GetMethods())
+            {
+                if (methodInfo.Name.ToLower().StartsWith(firstArgument) || methodInfo.Name.ToLower().Equals(firstArgument))
+                {
+                    ParameterInfo[] parameterInfo = methodInfo.GetParameters();
+                    if (parameterInfo.Length == 0)
+                    {
+                        // there is no parameter we can call with 'null'
+                        Console.WriteLine(methodInfo.Invoke(classInstance, null));
+                    }
+                    else
+                    {
+                        Console.WriteLine(methodInfo.Invoke(classInstance, new object[] { args[1] }));
+                    }
+                }
+            }
+        }
+    }
+}
